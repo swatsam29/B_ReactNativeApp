a58848f361fd9b9b975893bfa88967f56c1c512a
ssampathkumar <ssampathkumar@uco.edu>
2024-02-15 21:09:19 -0500

    commit

--- Home.js
+++ Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hey B! 🤓</Text>
-      {/* <Image
-        source={{
-          uri: 'file:///Users/swati/B_ReactNativeApp/images/HopelessR.jpeg',
-        }} */}
-        style={{ width: 410, height: 350 }} />
+    
       <Button
         title="Go to Profile"
         onPress={() => navigation.navigate('Profile')}
